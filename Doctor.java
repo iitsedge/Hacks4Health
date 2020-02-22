5f85c496c163324d54d01f82ba77accf70fb8d0e
megbooher <booher.j.megan@gmail.com>
2020-02-22 14:33:09 -0700

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,28 @@
+
+public class Doctor {
+	int hospID;
+	int drID;
+	boolean writePermission;
+		
+//data setters
+	public void setHospID(int ID) {
+		hospID = ID;
+	}
+	public void setDrID(int ID) {
+		drID = ID;
+	}
+	public void setWritePermission(boolean write) {
+		writePermission = write;
+	}
+	
+//data getters
+	public int getHostID() {
+		return hospID;
+	}
+	public int getDrID() {
+		 return drID;
+	}
+	public boolean getWritePermission() {
+		return writePermission;
+	}
+}
