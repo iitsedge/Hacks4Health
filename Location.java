17f657e2a21c46e82080b21b1d8db1c2b4fb409d
megbooher <booher.j.megan@gmail.com>
2020-02-22 14:34:06 -0700

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,21 @@
+public class Location {
+	public double x;
+	public double y;
+	public String city;
+	public int pCount;
+	
+//data setters
+	public Location(double x_, double y_, String city_) {
+		this.x = x_;
+		this.y = y_;
+		this.city = city_;
+		this.pCount = 0;
+	}
+	
+	public void addCount() {
+		pCount++;
+	}
+	public int getCount() {
+		return pCount;
+	}
+}
